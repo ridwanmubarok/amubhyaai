41bbe11a9a30e845137835ddecc7894ea19f5751
ridwanmubarok <ridwanmubarok5@gmail.com>
2023-07-19 22:40:11 +0700

    add

---
+++ apikey.py
@@ -0,0 +1,3 @@
+apikey = "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"
+serpapikey = "1e993481aa752e1ee4648738a7aae7ae806270f54ba07f51f2b29b8eaae7382a"
+pineconekey = "1d2aa92a-ea0f-4f3a-9a2a-07e4042c5be7"
